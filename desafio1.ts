a14faafbb89b83bdf24a463a73858621cdc70c18
matheushassen <matheushassen@hotmail.com>
2022-07-08 10:45:28 -0300

    initial comit

---
+++ desafio1.ts
@@ -0,0 +1,13 @@
+/* Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee = {};
+employee.code = 10;
+employee.name = "John";
+*/
+
+// RESOLVENDO O DESAFIO
+
+let employee:{code: number, name:string} = {
+    code: 10,
+    name: "John"
+}
---
+++ desafio3/desafio3.js
@@ -0,0 +1,58 @@
+/* O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
+
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
+let soma = document.getElementById('soma');
+let campoSaldo = document.getElementById('campo-saldo');
+
+campoSaldo.innerHTML = 0
+
+function somarAoSaldo(soma) {
+    campoSaldo.innerHTML += soma;
+}
+
+function limparSaldo() {
+    campoSaldo.innerHTML = '';
+}
+
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(soma.value);
+});
+
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
+
+/**
+    <h4>Valor a ser adicionado: <input id="soma"> </h4>
+    <button id="atualizar-saldo">Atualizar saldo</button>
+    <button id="limpar-saldo">Limpar seu saldo</button>
+    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
+ */
+// RESOLVENDO O DESAFIO
+var botaoAtualizar = document.getElementById('atualizar-saldo');
+var botaoLimpar = document.getElementById('limpar-saldo');
+var soma = document.getElementById('soma');
+var campoSaldo = document.getElementById('campo-saldo');
+var saldoTotal = 0;
+function somarAoSaldo(soma) {
+    if (campoSaldo) {
+        saldoTotal += soma;
+        campoSaldo.innerHTML = saldoTotal.toString();
+    }
+}
+function limparSaldo() {
+    if (campoSaldo) {
+        var saldoTotal_1 = 0;
+        campoSaldo.innerHTML = saldoTotal_1.toString();
+    }
+}
+botaoAtualizar === null || botaoAtualizar === void 0 ? void 0 : botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(Number(soma.value));
+});
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
+/**
+
+ */
